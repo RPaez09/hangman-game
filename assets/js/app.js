3cbc74f7bf7ea92f3a1c5cb960808ff64a52a785
Richard Paez <Richardp809@gmail.com>
2017-12-12 12:54:54 -0500

    Basic game object created

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+var game = {
+
+    wins : 0,
+    // how many wins the player has
+
+    dictionary : 
+    [ 
+        "Love" ,
+        "Breaking Bad" ,
+        "Better Call Saul" ,
+        "Black Mirror" ,
+        "Orange is the New Black" ,
+        "The Office" ,
+        "Stranger Things" ,
+        "Dear White People" , 
+        "Daredevil" , 
+        "Jessica Jones" , 
+        "The Defenders" , 
+        "Luke Cage" , 
+        "Flaked" , 
+        "Mindhunter" ,
+        "Narcos"
+    ],
+    // dictionary of possible matches
+
+    
+}
