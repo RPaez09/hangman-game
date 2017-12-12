50d71796364bfb0ec6435485aa0f1bd07d820f67
Richard Paez <Richardp809@gmail.com>
2017-12-12 13:08:59 -0500

    expanded game object

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ var game = {
     wins : 0,
     // how many wins the player has
 
+    remainingGuesses : 10,
+
     dictionary : 
     [ 
         "Love" ,
@@ -21,7 +23,19 @@ var game = {
         "Mindhunter" ,
         "Narcos"
     ],
-    // dictionary of possible matches
+    // dictionary of possible matches. not to be modified
 
-    
+    remainingWords : [],
+    //pool of words to select from
+
+    initialize : function(){
+
+        for( var a = 0; a < this.dictionary.length(); a++ ) {
+            this.remainingWords.push(this.dictionary[a]);
+        } // resets remaining words
+
+        this.remainingGuesses = 10;
+
+        this.wins = 0;
+    }
 }
